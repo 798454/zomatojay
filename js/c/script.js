58b3063b89909a6a6836d835a8ceafeddef49f30
Jay <jaypatel24101@gmail.com>
2025-08-18 13:46:24 +0530

    tomato web

---
+++ js/c/script.js
@@ -0,0 +1,49 @@
+console.log("hello world");
+console.warn("hello this is warniung");
+console.error("tusss");
+console.clear("helllo");
+console.assert(4 == 6);
+
+
+
+var number1 = 26;
+var number2 = 32;
+console.log(number1 + number2);
+
+
+var marks = {
+    jay : 99 ,
+    Nilesh : 97,
+    harshil :94,
+}
+console.log(marks)
+
+
+var a = true;
+var b = false;
+var c = null;
+
+console.log(a, b, c)
+
+
+var und;
+console.log(und)
+
+
+var arr = [1,2,"Jay Patel",4,5]
+console.log(arr[2])
+
+
+console.log(false && true)
+console.log(false || true)
+
+var a = 10;
+var b = a;
+b -= 2;
+console.log(b)
+
+
+var a = 10;
+var b = 20;
+console.log(a<=b);
+console.log(a=b);
